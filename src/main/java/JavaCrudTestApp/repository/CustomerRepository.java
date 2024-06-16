99d93c5103e92a159aeb8726bb9788837237cd34
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-17 01:43:12 +0300

    added: Customer repo

---
+++ src/main/java/JavaCrudTestApp/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package JavaCrudTestApp.repository;
+
+import JavaCrudTestApp.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
