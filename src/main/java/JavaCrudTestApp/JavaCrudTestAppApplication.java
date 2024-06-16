8c00a0b313d6cfe3b545e85fc9f3e603d09c6b0d
Ghostyshka <74868817+Ghostyshka@users.noreply.github.com>
2024-06-16 21:53:13 +0300

    Initial commit

---
+++ src/main/java/JavaCrudTestApp/JavaCrudTestAppApplication.java
@@ -0,0 +1,13 @@
+package JavaCrudTestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCrudTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCrudTestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JavaCrudTestApp/JavaCrudTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package JavaCrudTestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCrudTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
